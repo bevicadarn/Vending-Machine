15f57e5d1026a24273705d043f94e0649f94ee04
bevicadarn <bevicadarn@hotmail.com>
2017-08-30 16:37:01 +0100

    Update CoinCase.java

--- src/com/sdg/MachineBase/CoinCase.java
+++ src/com/sdg/MachineBase/CoinCase.java
@@ -1,31 +1,41 @@
 package com.sdg.MachineBase;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CoinCase {
 
 	// array of number of each coins and potential valkues
-	private int [] noOfCoins = new int[8];
-	private double [] values = new double[]{0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0};
+	private int [] noOfCoins = new int[CoinType.values().length];
+
 	
-	private int onePencePos = 1;
-	private int twoPencePos = 2;
-	private int fivePencePos = 3;
-	private int tenPencePos = 4;
-	private int twentyPencePos = 5;
-	private int fiftyPencePos = 6;
-	private int onePoundPos = 7;
-	private int twoPoundPos = 8; 
+	ArrayList<String> coinNames = new ArrayList<String>();
+
 	
 	public CoinCase() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	
+	public void addCoin(Coin newCoin) {
+		for (int i = 0; i < CoinType.values().length; i++) {
+
+			if (newCoin.getValue() == CoinType.values()[i].getEnumValue()) {
+				noOfCoins[i] ++;
+				
+			}
+			System.out.println(" noOfCoins  " + i + " is " + noOfCoins[i]);
+		}
+	}
 	
 	public double getTotalBanked() {
 		double total = 0.00;
+		
+		
 		for (int i = 0; i < values.length; i++) {
 			total += noOfCoins[i] * values[i];
 		}
 		return total;
 	}
+
 }
