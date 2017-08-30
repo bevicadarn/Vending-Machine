d5c14788d119988b6d4ed274b3964939072af75e
bevicadarn <bevicadarn@hotmail.com>
2017-08-30 16:40:27 +0100

    Update VendingMachine.java

--- src/com/sdg/MachineBase/VendingMachine.java
+++ src/com/sdg/MachineBase/VendingMachine.java
@@ -6,4 +6,73 @@ public class VendingMachine implements StockHandling {
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public void addStock() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeStock() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void countMoneyIn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void bankMoney() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cancelPurchase() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void returnChange() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void vendProduct() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkStockLevel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkMoneyLevel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addProductSlot() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeProductSlot() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+	
 }
